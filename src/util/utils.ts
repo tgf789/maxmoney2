f5b46ebdc7f2f101ed8f597b0d69671e5324d8a5
손영한 <tgf789@naver.com>
2023-03-14 17:58:29 +0900

    ㅇㅇㅇㅇ

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -15,3 +15,19 @@ export const parserResult = (n1:number,n2:number,parser:PARSER) => {
   if(PARSER.LTE === parser) return n1 <= n2 
   return false 
 }
+
+
+
+const fn = (price:number, amount:number|null) => {
+  let result = 0;
+  if (amount === null) {
+    amount = Math.min(Math.floor(100000/price), 100000);
+  } else {
+    result = price * amount;
+    if (result > 100000) {
+      amount = Math.floor(100000/price);
+      result = price * amount;
+    }
+  }
+  return result;
+}
